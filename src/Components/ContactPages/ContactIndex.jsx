9755154d157f20b6c6080bd9b227e2f88f4aa5c5
AnikoShaverdashvili <79081549+AnikoShaverdashvili@users.noreply.github.com>
2023-05-06 17:25:43 +0400

    add AddContact functionality

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../Layout/Header";
 import AddRandomContact from "./AddRandomContact";
 import RemoveAllContact from "./RemoveAllContact";
@@ -7,71 +7,81 @@ import FavoriteContacts from "./FavoriteContacts";
 import GeneralContacts from "./GeneralContacts";
 import Footer from "../Layout/Footer";
 
-class ContactIndex extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contactList: [
-        {
-          id: 1,
-          name: "Aniko Shaverdashvili",
-          phone: "551285085",
-          email: "shaverdashviliania@gmail.com",
-          isFavorite: false,
-        },
-        {
-          id: 2,
-          name: "Luka Shaverdashvili",
-          phone: "551278623",
-          email: "shaverdashvililuka@gmail.com",
-          isFavorite: true,
-        },
-        {
-          id: 3,
-          name: "Irakli Shaverdashvili",
-          phone: "5512786423",
-          email: "shaverdashviliirakli@gmail.com",
-          isFavorite: true,
-        },
-      ],
-    };
-  }
+const ContactIndex = () => {
+  const [contactList, setContactList] = useState([
+    {
+      id: 1,
+      name: "Aniko Shaverdashvili",
+      phone: "551285085",
+      email: "shaverdashviliania@gmail.com",
+      isFavorite: false,
+    },
+    {
+      id: 2,
+      name: "Luka Shaverdashvili",
+      phone: "551278623",
+      email: "shaverdashvililuka@gmail.com",
+      isFavorite: true,
+    },
+    {
+      id: 3,
+      name: "Irakli Shaverdashvili",
+      phone: "5512786423",
+      email: "shaverdashviliirakli@gmail.com",
+      isFavorite: true,
+    },
+  ]);
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="container" style={{ minHeight: "85vh" }}>
-          <div className="row py-3">
-            <div className="col-4 offset-2">
-              <AddRandomContact />
-            </div>
-            <div className="col-4">
-              <RemoveAllContact />
-            </div>
+  const handleAddRandomContact = () => {};
+  const handleRemoveContact = () => {};
+  const handleAddContact = (newContact) => {
+    const newFinalContact = {
+      ...newContact,
+      id: contactList[contactList.length - 1].id + 1,
+      isFavorite: false,
+    };
+    setContactList((prevState) => prevState.concat([newFinalContact]));
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="container" style={{ minHeight: "85vh" }}>
+        <div className="row py-3">
+          <div className="col-4 offset-2">
+            <AddRandomContact />
           </div>
-          <div className="py-2 col-12">
-            <AddContact />
-          </div>
-          <div className="py-2 col-12">
-            <FavoriteContacts
-              contacts={this.state.contactList.filter(
-                (u) => u.isFavorite == true
-              )}
-            />
-          </div>
-          <div className="py-2 col-12">
-            <GeneralContacts
-              contacts={this.state.contactList.filter(
-                (u) => u.isFavorite == false
-              )}
-            />
+          <div className="col-4">
+            <RemoveAllContact />
           </div>
         </div>
-        <Footer />
+        <div className="py-2 col-12">
+          <div className="col-8 offset-2 row">
+            <AddContact handleAddContact={handleAddContact} />
+          </div>
+        </div>
+        <div className="py-2 col-12">
+          <div className="col-8 offset-2 row">
+            <div className="col-12">
+              <FavoriteContacts
+                contacts={contactList.filter((u) => u.isFavorite === true)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="py-2 col-12">
+          <div className="col-8 offset-2 row">
+            <div className="col-12">
+              <GeneralContacts
+                contacts={contactList.filter((u) => u.isFavorite === false)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default ContactIndex;
